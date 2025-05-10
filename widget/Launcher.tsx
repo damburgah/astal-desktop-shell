de4d6894a8ae7747a3cd17ecf58daed319a723e7
jeff <jeff@nixos.dev>
2025-05-10 21:29:09 +0700

    nix icon name

--- widget/Launcher.tsx
+++ widget/Launcher.tsx
@@ -2,9 +2,6 @@ export default function Launcher() {
     return <button
         onClicked="echo launching"
         hexpand>
-            {/* <image iconName="system-run-symbolic" /> */}
-            {/* <image iconName="nix-symbolic" /> */}
             <image iconName="nix" />
-            {/* <image file ="assets/nix-symbolic.svg" /> */}
     </button>
 }
