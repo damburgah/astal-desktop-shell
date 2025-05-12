0cfc2378f2c1f900807877795e38a089df900fad
jeff <jeff@nixos.dev>
2025-05-12 20:14:15 +0700

    revert workspaces

--- widget/Workspaces.tsx
+++ widget/Workspaces.tsx
@@ -1,4 +1,3 @@
-import { Gtk } from "astal/gtk4"
 import { bind, Variable } from "astal"
 import Hyprland from "gi://AstalHyprland"
 
@@ -6,30 +5,21 @@ const hypr = Hyprland.get_default()
 
 const wsLabels = ["α", "β", "γ", "δ", "ε", "ζ"]
 
-function WorkspaceButton(index: number, label: string) {
-    const classNames = Variable.derive(
-        [bind(hypr, "focusedWorkspace")],
-        (fw) => {
-            const classes = ["Workspace"]
-            if (fw?.id === index + 1) classes.push("Active")
-            return classes
-        }
-    )
-
-    function setup(button: Gtk.Button) {
-        button.hexpand = true
-        button.child = new Gtk.Label({ label })
-        button.connect("clicked", () => "echo clicked")
-        button.cssClasses = classNames.get()
-
-        classNames.subscribe(classes => {
-            button.cssClasses = classes
-        })
-    }
-
-    return <button setup={setup} />
-}
-
 export default function Workspaces() {
-    return wsLabels.map((label, i) => WorkspaceButton(i, label))
+    return wsLabels.map((label, i) => {
+        const classNames = Variable.derive(
+            [bind(hypr, "focusedWorkspace")
+        ], (fw) => {
+            const classes = ["Workspace"]
+            if (fw.id === i + 1) classes.push("Active")
+            return classes
+        })
+
+        return <button
+                cssClasses={classNames()}
+                onClicked="echo launching"
+                hexpand>
+                <label label={label} />
+        </button>
+    })
 }
